62caaaf2daaba52060557cc616157743ae402835
Patrick <patrickvandesluis@bitbucket.org>
2021-04-11 20:46:38 +0200

    v0.2

--- shrtnr/forms.py
+++ shrtnr/forms.py
@@ -1,6 +1,19 @@
 from django import forms
+from .models import Link
+import re
 
 
 class LinkForm(forms.Form):
+    def clean_short(self):
+        short = self.cleaned_data['short']
+        if Link.objects.filter(short=short).exists():
+            raise forms.ValidationError("Short already exists")
+        return short
+    def clean_link(self):
+        link = self.cleaned_data['link']
+        if not re.match('((http|https)\:\/\/)?[a-zA-Z0-9\.\/\?\:@\-_=#]+\.([a-zA-Z]){2,6}([a-zA-Z0-9\.\&\/\?\:@\-_=#])*', link):
+            raise forms.ValidationError("Link is not a valid URL")
+        return link
     link = forms.CharField(max_length=5000, label="LINK")
     short = forms.CharField(max_length=200, label="SHORT", required=False)
+
--- shrtnr/views.py
+++ shrtnr/views.py
@@ -16,29 +16,26 @@ def get_client_ip(request):
 
 
 def index(request):
+    context = {}
     if request.method == 'POST':
-        form = LinkForm(request.POST)
-        print(form.is_valid())
-        if form.is_valid():
-            link = form.cleaned_data["link"]
-            if re.match('((http|https)\:\/\/)?[a-zA-Z0-9\.\/\?\:@\-_=#]+\.([a-zA-Z]){2,6}([a-zA-Z0-9\.\&\/\?\:@\-_=#])*', link):
-                short = ''.join(random.choice(string.ascii_letters) for x in range(10))
-                ip = get_client_ip(request)
-                new_url = Link(link=link, short=short, ip=ip)
-                new_url.save()
-                return render(request, 'index.html', {
-                    'url': request.build_absolute_uri() + new_url.short,
-                    'org_url': new_url.link
-                    })
+        context['form'] = LinkForm(request.POST)
+        if context['form'].is_valid():
+            link = context['form'].cleaned_data["link"]
+            if not link.startswith('http'):
+                link = 'http://' + link
+            if context['form'].cleaned_data["short"]:
+                print('has short')
+                short = context['form'].cleaned_data["short"]
             else:
-                print('False')
+                short = ''.join(random.choice(string.ascii_letters) for x in range(10))
+            ip = get_client_ip(request)
+            new_url = Link(link=link, short=short, ip=ip)
+            new_url.save()
+            context['url'] = request.build_absolute_uri() + new_url.short,
+            context['org_url'] = new_url.link
     else:
-        form = LinkForm()
-    data = Link.objects.all()
-    context = {
-        'form': form,
-        'data': data
-    }
+        context['form'] = LinkForm()
+
     return render(request, 'index.html', context)   
 
 
